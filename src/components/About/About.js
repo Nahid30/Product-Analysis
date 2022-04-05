76895ac952f6b9cc830eff559fed8b37d2978166
Nahid30 <nahidshuvo30@gmail.com>
2022-04-05 14:07:08 +0600

    answered of two question and corrected about part

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,13 @@ const About = () => {
         <div className='about-container'>
             <h1>We are Number one Watch brand shop in your Town , You already heard about us thats why you're reading this!!</h1>
 
-            <p>Thanks For Buying!!</p>
+            <h4>Thanks For Buying!!</h4>
+
+            <div>
+                <p>Name : Watch House</p>
+                <p>Phone : 96482366</p>
+                <p>Address: 36/4 New York , US</p>
+            </div>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -7,12 +7,12 @@ const Blogs = () => {
 
             <div className='question-one'>
                 <h2>What is Context Api?</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In repellat itaque cum vitae? Sapiente, possimus velit. Sapiente officiis odit asperiores iste quibusdam beatae voluptatum consequuntur, voluptas, placeat vel quae accusantium!</p>
+                <p>Context API helps passing data to child component without pops drilling. It is like mobile network. By wrapping context API component any data can be passed through Context API to the child components or file only. It creates a context and export to share file to another file. It provides two components Provider and Consumer wrapped by Context API component. Last, in the Provider component any dynamic or static data can pass through value, and catch from child component.</p>
             </div>
             
             <div className='question-two'>
                 <h2>What is Semantic tag?</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus quibusdam molestias numquam quia suscipit laboriosam quidem. Qui quam magnam facilis. Numquam pariatur quo suscipit optio non facilis doloremque aperiam fugiat?</p>
+                <p>Semantic tags are meaningful html tags. By reading tag we can understand the purpose of that tag. Like - section, article, main, strong, bold etc.</p>
             </div>
 
         </div>
